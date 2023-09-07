bcdfa91e212217268a067c7bcac49cf2754a92d5
hlibyelkhov <hlib.yelkhov@gmail.com>
2023-09-07 14:36:48 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box>
+
+      </Box>
     </div>
   );
 }
+
+const Box = styled.div`
+  height: 350px;
+  width: 300px;
+  border-radius: 15px;
+  background-color: #fff;
+`
 
 export default App;
