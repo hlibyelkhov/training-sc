86f01d7c14d97d1d45716918af180eaaccb3e11b
hlibyelkhov <hlib.yelkhov@gmail.com>
2023-09-11 17:48:04 +0200

    Text and Button file

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import styled from "styled-components";
 import {Box} from "./components/Box.styled";
 import Img from './images/Rectangle 1.png';
+import {StyledBtn} from "./components/Button.styled";
 
 function App() {
     return (
@@ -12,6 +13,8 @@ function App() {
                     <img src={Img} alt=""/>
                     <h1>Headline</h1>
                     <p>Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in venen.</p>
+                    <StyledBtn>See more</StyledBtn>
+                    <StyledBtn>Save</StyledBtn>
                 </Box>
             </Container>
         </div>
---
+++ src/components/Button.styled.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const StyledBtn = styled.button `
+  width: 86px;
+  height: 30px;
+  border: 2px solid #4E71FE;
+  border-radius: 5px;
+  font-family: Inter;
+  font-weight: 700;
+  font-size: 10px;
+  color: #4E71FE;
+  margin-left: 20px;
+
+  &:hover {
+    border-color: #4E71FE;
+    color: #fff;
+    background-color: #4E71FE;
+  }
+`
